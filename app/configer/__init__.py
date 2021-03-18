ee4efe585bf58fc54adfe7e4b24b726d81e2fcc1
Denis Belavin <belavin.d.andreevich@gmail.com>
2021-03-18 09:15:16 +0300

    Module 'app.config' changes. Failure to use the module 'app.configer.variables', poor design. Adding and semantic editing of comments.

--- app/configer/__init__.py
+++ app/configer/__init__.py
@@ -3,16 +3,10 @@
 from .file_reader import BaseFileReader
 from .file_reader import YAMLFileReader
 from .reader import ConfigReader
-from .variables import AiogramVariables
-from .variables import BaseVariables
-from .variables import ConfigVariables
 
 
 __all__ = [
     BaseFileReader,
     YAMLFileReader,
-    ConfigReader,
-    AiogramVariables,
-    ConfigVariables,
-    BaseVariables
+    ConfigReader
 ]
--- app/configer/reader.py
+++ app/configer/reader.py
@@ -1,16 +1,14 @@
 # coding: utf-8
 
-from typing import AnyStr, Dict, Iterable
+from typing import AnyStr, Dict, Type
 
 from .file_reader import BaseFileReader
 from .file_reader import YAMLFileReader
 from .utils import get_from_environ
-from .variables import AiogramVariables
-from .variables import ConfigVariables
 
 
 class ConfigReader:
-    FILE_READER = YAMLFileReader
+    FILE_READER: Type[BaseFileReader] = YAMLFileReader
 
     def __init__(self):
         self.data: Dict = dict()
@@ -18,66 +16,96 @@ class ConfigReader:
         self._logging: Dict = dict()
 
     def setup(self) -> 'ConfigReader':
-        file_reader = self.FILE_READER
-        if not issubclass(file_reader, BaseFileReader):
-            raise TypeError(f'config_reader.FILE_READER is not '
-                            f'child of {type(BaseFileReader)}')
+        """
+        Настройка и чтения файла конфигурации.
 
-        self.data = self.FILE_READER().setup().read()
+        Пример:
+        config_reader = ConfigReader().setup()
+
+        Пример:
+        config_reader = ConfigReader()
+        config_reader.setup()
+
+        :return: ConfigReader
+        """
+        file_reader = self.FILE_READER()
+        if not isinstance(file_reader, BaseFileReader):
+            raise TypeError(f'config_reader.FILE_READER={type(file_reader)} '
+                            f'не является типом {BaseFileReader}')
+
+        self.data = file_reader.setup().read()
         self._aiogram = {}
         self._logging = {}
         return self
 
-    def merge_variables(self, var_list: Iterable, section_name: AnyStr) -> Dict:
+    def mount_section(self, section_name: AnyStr, env_merge: bool = False) -> Dict:
         """
-        Переменные окружения имеют более высоки приоритет,
-        по этому перезаписываем переменные из файла конфигурации.
+        Подключает секцию с переменными.
 
-        :param var_list: dict
-        :param section_name:
-        :return:
+        Флаг `env_merge=True` указывает на логику обработки
+        значений которые будут храниться в подключенной секции.
+        Так как не для всех секций необходимо слияние значений
+        переменных из файла конфигурации и значения переменных
+        окружения.
+
+        Переменные окружения имеют более высоки приоритет, по этому
+        перезаписывают значения переменных из файла конфигурации.
+
+        :param section_name: str
+        :param env_merge: bool, default = False
+
+        :return: dict
         """
-        env_vars = {}
-        for var in var_list:
-            env_vars[var] = get_from_environ(var)
+        section = self.data[section_name]
+        if not env_merge:
+            return section
 
-        cnf_vars = self.data[str(section_name)]
-        for var, val in env_vars.items():
-            if val:
-                cnf_vars[var] = val
-        return cnf_vars
+        for variable in section:
+            value = get_from_environ(variable)
+            if value:
+                section[variable] = value
+        return section
 
-    def version(self):
-        return self.data[str(ConfigVariables.VERSION)]
-
-    def aiogram(self, var) -> AnyStr:
+    def version(self) -> AnyStr:
         """
-        Отдаем строку содержащие значение, переменной настройки,
-        так как требуется немного разная настройка для методов
-        `start_polling` и `start_webhook`.
+        Версия файла конфигурации.
+        1.2.3
+        x.0.0 - major
+        1.x.0 - minor
+        1.2.x - patch
+
+        :return: str
+        """
+        return self.data['VERSION']
+
+    def aiogram(self, var: AnyStr) -> AnyStr:
+        """
+        Отдаем значение, а не словарь, так как
+        требуется разный набор параметров для
+        настройка методов `start_polling` и
+        `start_webhook`.
 
         :param var: str
         :return: str
         """
         if not self._aiogram:
-            self._aiogram = self.merge_variables(
-                AiogramVariables,
-                str(ConfigVariables.AIOGRAM)
+            self._aiogram = self.mount_section(
+                'aiogram', env_merge=True
             )
 
-        return self._aiogram[str(var)]
+        return self._aiogram[var]
 
     def logging(self) -> Dict:
         """
         Отдаем сразу словарь, так как интерфейс
         библиотеки `logging` позволяет настройку
         через словарь.
+        Подробнее: `logging.config.dictConfig`.
 
         :return: dict
         """
         if not self._logging:
-            self._logging = self.merge_variables(
-                dict(),
-                str(ConfigVariables.LOGGING)
+            self._logging = self.mount_section(
+                'logging', env_merge=False
             )
         return self._logging
